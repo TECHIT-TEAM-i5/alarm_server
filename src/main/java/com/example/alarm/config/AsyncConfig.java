c158e2a11657a9c688cf095dddd7d009ee8dc58d
joshiaLee <hhhjs0133@naver.com>
2024-04-25 07:37:48 +0900

    Fix: use a session to maintain the connection with Google

--- src/main/java/com/example/alarm/config/AsyncConfig.java
+++ src/main/java/com/example/alarm/config/AsyncConfig.java
@@ -18,8 +18,8 @@ public class AsyncConfig implements AsyncConfigurer {
     int processors = Runtime.getRuntime().availableProcessors(); // 내 PC의 Processor 개수를 가져옴.
     ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor(); // Thread Pool 관리 클래스
     executor.setCorePoolSize(processors); // 기본 스레드 개수
-    executor.setMaxPoolSize(processors * 2); // 최대 스레드 개수 (Queue가 가득찬 이후 maxPoolSize만큼 생성)
-    executor.setQueueCapacity(50); // 대기를 위한 Queue 크기
+    executor.setMaxPoolSize(15000); // 최대 스레드 개수 (Queue가 가득찬 이후 maxPoolSize만큼 생성)
+    executor.setQueueCapacity(15000); // 대기를 위한 Queue 크기
     executor.setKeepAliveSeconds(60);  // 스레드 재사용 시간
     executor.setThreadNamePrefix("AsyncExecutor-"); // 스레드 이름 prefix
     executor.initialize(); // ThreadPoolExecutor 생성
--- src/main/java/com/example/alarm/service/EmailAlertService.java
+++ src/main/java/com/example/alarm/service/EmailAlertService.java
@@ -3,14 +3,17 @@ package com.example.alarm.service;
 import com.example.alarm.dto.AlertDto;
 import com.example.alarm.dto.UserMailDto;
 import com.google.gson.Gson;
-import jakarta.mail.MessagingException;
+import jakarta.annotation.PostConstruct;
+import jakarta.mail.*;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
@@ -18,12 +21,13 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class EmailAlertService implements AlertService {
-    private final JavaMailSender mailSender;
+    private JavaMailSenderImpl mailSender;
     private final SpringTemplateEngine templateEngine;
 
 
@@ -31,14 +35,64 @@ public class EmailAlertService implements AlertService {
     private final Gson gson;
     private static final String EMAIL_TITLE_PREFIX = "[STAGE ALARM] 알림 : 새 공연이 등록되었습니다. ";
 
+    private Session session;
+    private Transport transport;
+    @PostConstruct
+    private void initializeMailSender() throws MessagingException {
+        mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("youshin.dev@gmail.com");
+        mailSender.setPassword("tdin xdoe suwi fbok");
 
-    @Override
-    @RabbitListener(queues = "boot.amqp.alarm-queue")
+        Properties props = mailSender.getJavaMailProperties();
+
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", 587);
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.starttls.enable", true);
+        props.put("mail.smtp.connectiontimeout", 10000);
+        props.put("mail.smtp.timeout", 10000);
+        // SMTP Keep-Alive 활성화
+        props.put("mail.smtp.keepalive", true); // Keep-Alive 활성화
+
+
+
+
+        session = Session.getInstance(props, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(mailSender.getUsername(), mailSender.getPassword());
+            }
+        });
+        transport = session.getTransport("smtp");
+    }
+
+    @RabbitListener(queues = "boot.amqp.alarm2-queue")
+    @Retryable(value = MessagingException.class, maxAttempts = 3, backoff = @Backoff(delay = 10000))
     @Async("threadPoolTaskExecutor")
     public void sendMail(String rabbitMessage) throws MessagingException {
-        log.info("===== email sending start =====");
-        AlertDto alertDto = gson.fromJson(rabbitMessage, AlertDto.class);
-        log.info(alertDto.toString());
+        try {
+            log.info("===== email sending start =====");
+            if (!transport.isConnected()) {
+                transport.connect();
+            }
+            AlertDto alertDto = gson.fromJson(rabbitMessage, AlertDto.class);
+            log.info(alertDto.toString());
+
+            sendMail(alertDto);
+
+        } finally {
+            if (rabbitMessage == null || rabbitMessage.isEmpty()) {
+                // 메시지 큐가 비어있으면 연결을 닫습니다.
+                transport.close();
+            }
+        }
+    }
+
+
+
+    public void sendMail(AlertDto alertDto) throws MessagingException {
+
 
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
@@ -53,12 +107,12 @@ public class EmailAlertService implements AlertService {
         helper.addInline("logo", new ClassPathResource("static/images/logo.png"));
         helper.addInline("notice-icon", new ClassPathResource("static/images/image-1.png"));
 
-        mailSender.send(message);
+        transport.sendMessage(message, message.getAllRecipients());
         log.info("===== email sending end =====");
     }
 
     @Override
-    @RabbitListener(queues = "boot.amqp.auth-queue")
+    @RabbitListener(queues = "boot.amqp.auth2-queue")
     @Async("threadPoolTaskExecutor")
     public void sendAuthMail(String authMessage) throws MessagingException {
         log.info("===== auth email sending start =====" );
